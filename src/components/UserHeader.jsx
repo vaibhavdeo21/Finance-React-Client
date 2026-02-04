f5c4a9d30569d6e6f3f68c205502c73bd41ecfe1
Vaibhav Pratap Deo <vaibhavpdeo0028@gmail.com>
2026-02-04 10:24:21 +0530

    Gets user from Store via useSelector

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -1,34 +1,31 @@
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 function UserHeader() {
+  const user = useSelector((state) => state.userDetails);
+
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/dashboard">
-          ExpenseApp
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+    <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+      <div className="container">
+        <Link className="navbar-brand" to="/dashboard">ExpenseApp</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
           <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/dashboard">
-                Dashboard
+             <li className="nav-item">
+                <Link className="nav-link" to="/groups">My Groups</Link>
+             </li>
+          </ul>
+          
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown">
+              <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                {user ? user.name : "Account"}
               </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/logout">
-                Logout
-              </Link>
+              <ul className="dropdown-menu dropdown-menu-end">
+                <li><Link className="dropdown-item" to="/logout">Logout</Link></li>
+              </ul>
             </li>
           </ul>
         </div>
