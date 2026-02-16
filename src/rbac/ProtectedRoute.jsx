51f1a58ce50a780edfa39b69bf5e7e86d1e93133
vaibhavdeo21 <vaibhavpdeo0028@gmail.com>
2026-02-16 21:30:02 +0530

    minor changes

--- src/rbac/ProtectedRoute.jsx
+++ src/rbac/ProtectedRoute.jsx
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ roles, children }) {
-    const user = useSelector(state => state.userDetails);
     
     return roles.includes(user?.role) ? 
         children : 
