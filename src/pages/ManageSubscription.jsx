200a1ed1e4f14ecdf71baffb69056cbf563acc4e
vaibhavdeo21 <vaibhavpdeo0028@gmail.com>
2026-02-26 12:45:20 +0530

    minor change

--- src/pages/ManageSubscription.jsx
+++ src/pages/ManageSubscription.jsx
@@ -19,6 +19,7 @@ function ManageSubscription() {
     const [userProfile, setUserProfile] = useState(null);
     const [errors, setErrors] = useState({});
     const [message, setMessage] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const getUserProfile = async () => {
         try {
